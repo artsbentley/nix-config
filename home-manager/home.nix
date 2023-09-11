a2cccfc6295c8c61ac75cbb18cb6bd45bc673724
artsbentley <artsbentley@gmail.com>
2023-09-11 21:29:25 +0200

    test(module split): trying out the splitting of modules for the first time

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -75,23 +75,23 @@
   };
 
 
-programs.zsh = {
-  enable = true;
-  #dotDir = ".config/zsh";
-  shellAliases = {
-    "vim" = "nvim";
-    "v" = "nvim";
-    ":GoToFile" = "nvim +GoToFile";
-
-
-    ".." = "cd ..";
-    "c" = "clear";
-    "l" = "exa -lbF -l --icons -a --git";
-    "update" = "cd ~/nix-config && sudo nixos-rebuild switch --flake .#arar";
-  };
-  #histSize = 10000;
-  #histFile = "${config.xdg.dataHome}/zsh/history";
-};
+# programs.zsh = {
+#   enable = true;
+#   #dotDir = ".config/zsh";
+#   shellAliases = {
+#     "vim" = "nvim";
+#     "v" = "nvim";
+#     ":GoToFile" = "nvim +GoToFile";
+#
+#
+#     ".." = "cd ..";
+#     "c" = "clear";
+#     "l" = "exa -lbF -l --icons -a --git";
+#     "update" = "cd ~/nix-config && sudo nixos-rebuild switch --flake .#arar";
+#   };
+#   #histSize = 10000;
+#   #histFile = "${config.xdg.dataHome}/zsh/history";
+# };
 
 programs.zoxide = {
       enable = true;
