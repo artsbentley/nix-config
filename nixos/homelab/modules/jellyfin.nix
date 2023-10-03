98c061ad853f4e9338e497810b0262ea7bbf52b1
artsbentley <artsbentley@gmail.com>
2023-10-03 23:22:07 +0200

    jellyfin wip

--- nixos/homelab/modules/jellyfin.nix
+++ nixos/homelab/modules/jellyfin.nix
@@ -1,20 +1,42 @@
 { config, pkgs, ... }:
 
 {
-  networking.firewall = {
-    allowedTCPPorts = [
-      8096
-      8920 # Web frontend
+
+  # Jellyfin
+  virtualisation.oci-containers.containers."jellyfin" = {
+    autoStart = true;
+    image = "jellyfin/jellyfin";
+    volumes = [
+      "/media/Containers/Jellyfin/config:/config"
+      "/media/Containers/Jellyfin/cache:/cache"
+      "/media/Containers/Jellyfin/log:/log"
+      "/media/Movies:/movies"
+      "/media/TV-Series:/tv"
     ];
-    allowedUDPPorts = [
-      1900
-      7359 # Discovery
-    ];
+    ports = [ "8096:8096" ];
+    environment = {
+      JELLYFIN_LOG_DIR = "/log";
+    };
   };
 
-  services.jellyfin =
-    {
-      enable = true;
-      package = pkgs.jellyfin; # Upgrade to 10.6.x
-    };
-}
+}#
+# { config, pkgs, ... }:
+#
+# {
+#   networking.firewall = {
+#     allowedTCPPorts = [
+#       8096
+#       8920 # Web frontend
+#     ];
+#     allowedUDPPorts = [
+#       1900
+#       7359 # Discovery
+#     ];
+#   };
+#
+#   services.jellyfin =
+#     {
+#       enable = true;
+#       package = pkgs.jellyfin; # Upgrade to 10.6.x
+#     };
+# }
