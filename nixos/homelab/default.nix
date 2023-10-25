e6fd52698ae959a517e025d76c8dca93d3aee6ef
artsbentley <artsbentley@gmail.com>
2023-10-25 21:33:19 +0200

    wip

--- nixos/homelab/default.nix
+++ nixos/homelab/default.nix
@@ -12,8 +12,7 @@
     # ./users.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
-    ./modules/multimedia.nix
-    # ./modules/jellyfin.nix
+    ./modules/jellyfin.nix
     ./hardware-configuration.nix
     #NOTE this is used for combining home-manager into one
     # inputs.home-manager.nixosModules.home-manager
