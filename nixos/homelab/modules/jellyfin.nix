151357a26a04e7f2122d7a093ab50693ed6e4327
artsbentley <artsbentley@gmail.com>
2023-10-25 22:12:46 +0200

    wip

--- nixos/homelab/modules/jellyfin.nix
+++ nixos/homelab/modules/jellyfin.nix
@@ -1,52 +1,52 @@
+# { config, pkgs, ... }:
+
+# {
+# networking.firewall = {
+#   allowedTCPPorts = [
+#     8096
+#     8920 # Web frontend
+#   ];
+#   allowedUDPPorts = [
+#     1900
+#     7359 # Discovery
+#   ];
+# };
+
+#   # Jellyfin
+#   virtualisation.oci-containers.containers."jellyfin" = {
+#     autoStart = true;
+#     image = "jellyfin/jellyfin";
+#     volumes = [
+#       "/media/Containers/Jellyfin/config:/config"
+#       "/media/Containers/Jellyfin/cache:/cache"
+#       "/media/Containers/Jellyfin/log:/log"
+#       "/media/Movies:/movies"
+#       "/media/TV-Series:/tv"
+#     ];
+#     ports = [ "8096:8096" ];
+#     environment = {
+#       JELLYFIN_LOG_DIR = "/log";
+#     };
+#   };
+#
+# }
 { config, pkgs, ... }:
 
 {
-  # networking.firewall = {
-  #   allowedTCPPorts = [
-  #     8096
-  #     8920 # Web frontend
-  #   ];
-  #   allowedUDPPorts = [
-  #     1900
-  #     7359 # Discovery
-  #   ];
-  # };
-
-  # Jellyfin
-  virtualisation.oci-containers.containers."jellyfin" = {
-    autoStart = true;
-    image = "jellyfin/jellyfin";
-    volumes = [
-      "/media/Containers/Jellyfin/config:/config"
-      "/media/Containers/Jellyfin/cache:/cache"
-      "/media/Containers/Jellyfin/log:/log"
-      "/media/Movies:/movies"
-      "/media/TV-Series:/tv"
+  networking.firewall = {
+    allowedTCPPorts = [
+      8096
+      8920 # Web frontend
     ];
-    ports = [ "8096:8096" ];
-    environment = {
-      JELLYFIN_LOG_DIR = "/log";
-    };
+    allowedUDPPorts = [
+      1900
+      7359 # Discovery
+    ];
   };
 
+  services.jellyfin =
+    {
+      enable = true;
+      package = pkgs.jellyfin; # Upgrade to 10.6.x
+    };
 }
-# { config, pkgs, ... }:
-#
-# {
-#   networking.firewall = {
-#     allowedTCPPorts = [
-#       8096
-#       8920 # Web frontend
-#     ];
-#     allowedUDPPorts = [
-#       1900
-#       7359 # Discovery
-#     ];
-#   };
-#
-#   services.jellyfin =
-#     {
-#       enable = true;
-#       package = pkgs.jellyfin; # Upgrade to 10.6.x
-#     };
-# }
