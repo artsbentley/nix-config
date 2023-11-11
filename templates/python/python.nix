643e76cc9495288cca93ba48f39858cee3b33c05
Arno Arts <arar@arnos-mbp.home>
2023-11-11 21:49:13 +0100

    feat(python template): added initial flake for python projects

---
+++ templates/python/python.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "aarch64-darwin";
+      #       ↑ Swap it for your system if needed
+      #       "aarch64-linux" / "x86_64-darwin" / "aarch64-darwin"
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+
+        packages = with pkgs; [
+          python
+          poetry
+        ];
+        # ...
+
+      };
+    };
+}
+
+
+
+
