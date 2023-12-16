152aa419664e652a08d4ebcb1387bdf6fab0d797
artsbentley <artsbentley@gmail.com>
2023-12-16 18:18:14 +0100

    test

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Your new nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.1";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hardware.url = "github:nixos/nixos-hardware";
