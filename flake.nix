085a5edffac8e9f3b4bef259a4869b0697b072b0
artsbentley <artsbentley@gmail.com>
2023-12-16 18:17:19 +0100

    pp

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Your new nix config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.1";
       inputs.nixpkgs.follows = "nixpkgs";
