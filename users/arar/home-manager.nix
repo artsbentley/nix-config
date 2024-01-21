75978cd067587886559a047d7993f2b13820ee7f
artsbentley <artsbentley@gmail.com>
2024-01-21 20:45:57 +0100

    adjust makefile for hardware config mv

--- users/arar/home-manager.nix
+++ users/arar/home-manager.nix
@@ -8,11 +8,11 @@ let
 
   # For our MANPAGER env var
   # https://github.com/sharkdp/bat/issues/1145
-  manpager = (pkgs.writeShellScriptBin "manpager" (if isDarwin then ''
-    sh -c 'col -bx | bat -l man -p'
-  '' else ''
-    cat "$1" | col -bx | bat --language man --style plain
-  ''));
+  # manpager = (pkgs.writeShellScriptBin "manpager" (if isDarwin then ''
+  #   sh -c 'col -bx | bat -l man -p'
+  # '' else ''
+  #   cat "$1" | col -bx | bat --language man --style plain
+  # ''));
 in
 {
   # Home-manager 22.11 requires this be set. We never set it so we have
@@ -58,221 +58,221 @@ in
     pkgs.xfce.xfce4-terminal
   ]);
 
-  #---------------------------------------------------------------------
-  # Env vars and dotfiles
-  #---------------------------------------------------------------------
-
-  home.sessionVariables = {
-    LANG = "en_US.UTF-8";
-    LC_CTYPE = "en_US.UTF-8";
-    LC_ALL = "en_US.UTF-8";
-    EDITOR = "nvim";
-    PAGER = "less -FirSwX";
-    MANPAGER = "${manpager}/bin/manpager";
-  };
-
-  home.file.".gdbinit".source = ./gdbinit;
-  home.file.".inputrc".source = ./inputrc;
-
-  xdg.configFile = {
-    "i3/config".text = builtins.readFile ./i3;
-    "rofi/config.rasi".text = builtins.readFile ./rofi;
-
-  } // (if isDarwin then {
-    # Rectangle.app. This has to be imported manually using the app.
-    "rectangle/RectangleConfig.json".text = builtins.readFile ./RectangleConfig.json;
-  } else { }) // (if isLinux then {
-    "ghostty/config".text = builtins.readFile ./ghostty.linux;
-  } else { });
-
-  #---------------------------------------------------------------------
-  # Programs
-  #---------------------------------------------------------------------
-
-  programs.gpg.enable = !isDarwin;
-
-  programs.bash = {
-    enable = true;
-    shellOptions = [ ];
-    historyControl = [ "ignoredups" "ignorespace" ];
-    initExtra = builtins.readFile ./bashrc;
-
-    shellAliases = {
-      ga = "git add";
-      gc = "git commit";
-      gco = "git checkout";
-      gcp = "git cherry-pick";
-      gdiff = "git diff";
-      gl = "git prettylog";
-      gp = "git push";
-      gs = "git status";
-      gt = "git tag";
-    };
-  };
-
-  programs.direnv = {
-    enable = true;
-
-    config = {
-      whitelist = {
-        prefix = [
-          "$HOME/code/go/src/github.com/hashicorp"
-          "$HOME/code/go/src/github.com/mitchellh"
-        ];
-
-        exact = [ "$HOME/.envrc" ];
-      };
-    };
-  };
-
-
-  programs.git = {
-    enable = true;
-    userName = "Mitchell Hashimoto";
-    userEmail = "mitchell.hashimoto@gmail.com";
-    signing = {
-      key = "523D5DC389D273BC";
-      signByDefault = true;
-    };
-    aliases = {
-      cleanup = "!git branch --merged | grep  -v '\\*\\|master\\|develop' | xargs -n 1 -r git branch -d";
-      prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
-      root = "rev-parse --show-toplevel";
-    };
-    extraConfig = {
-      branch.autosetuprebase = "always";
-      color.ui = true;
-      core.askPass = ""; # needs to be empty to use terminal for ask pass
-      credential.helper = "store"; # want to make this more secure
-      github.user = "mitchellh";
-      push.default = "tracking";
-      init.defaultBranch = "main";
-    };
-  };
-
-  programs.go = {
-    enable = true;
-    goPath = "code/go";
-    goPrivate = [ "github.com/mitchellh" "github.com/hashicorp" "rfc822.mx" ];
-  };
-
-  programs.tmux = {
-    enable = true;
-    terminal = "xterm-256color";
-    shortcut = "l";
-    secureSocket = false;
-
-    extraConfig = ''
-      set -ga terminal-overrides ",*256col*:Tc"
-
-      set -g @dracula-show-battery false
-      set -g @dracula-show-network false
-      set -g @dracula-show-weather false
-
-      bind -n C-k send-keys "clear"\; send-keys "Enter"
-
-    '';
-  };
-
-  programs.alacritty = {
-    enable = !isWSL;
-
-    settings = {
-      env.TERM = "xterm-256color";
-
-      key_bindings = [
-        { key = "K"; mods = "Command"; chars = "ClearHistory"; }
-        { key = "V"; mods = "Command"; action = "Paste"; }
-        { key = "C"; mods = "Command"; action = "Copy"; }
-        { key = "Key0"; mods = "Command"; action = "ResetFontSize"; }
-        { key = "Equals"; mods = "Command"; action = "IncreaseFontSize"; }
-        { key = "Subtract"; mods = "Command"; action = "DecreaseFontSize"; }
-      ];
-    };
-  };
-
-  programs.kitty = {
-    enable = !isWSL;
-    extraConfig = builtins.readFile ./kitty;
-  };
-
-  programs.i3status = {
-    enable = isLinux && !isWSL;
-
-    general = {
-      colors = true;
-      color_good = "#8C9440";
-      color_bad = "#A54242";
-      color_degraded = "#DE935F";
-    };
-
-    modules = {
-      ipv6.enable = false;
-      "wireless _first_".enable = false;
-      "battery all".enable = false;
-    };
-  };
-
-  programs.neovim = {
-    enable = true;
-    package = pkgs.neovim-nightly;
-
-    withPython3 = true;
-
-    plugins = with pkgs; [
-      customVim.vim-copilot
-      customVim.vim-cue
-      customVim.vim-fish
-      customVim.vim-fugitive
-      customVim.vim-glsl
-      customVim.vim-misc
-      customVim.vim-pgsql
-      customVim.vim-tla
-      customVim.vim-zig
-      customVim.pigeon
-      customVim.AfterColors
-
-      customVim.vim-nord
-      customVim.nvim-comment
-      customVim.nvim-lspconfig
-      customVim.nvim-plenary # required for telescope
-      customVim.nvim-telescope
-      customVim.nvim-treesitter
-      customVim.nvim-treesitter-playground
-      customVim.nvim-treesitter-textobjects
-
-      vimPlugins.vim-airline
-      vimPlugins.vim-airline-themes
-      vimPlugins.vim-eunuch
-      vimPlugins.vim-gitgutter
-
-      vimPlugins.vim-markdown
-      vimPlugins.vim-nix
-      vimPlugins.typescript-vim
-    ] ++ (lib.optionals (!isWSL) [
-      # This is causing a segfaulting while building our installer
-      # for WSL so just disable it for now. This is a pretty
-      # unimportant plugin anyway.
-      customVim.vim-devicons
-    ]);
-  };
-
-  services.gpg-agent = {
-    enable = isLinux;
-    pinentryFlavor = "tty";
-
-    # cache the keys forever so we don't get asked for a password
-    defaultCacheTtl = 31536000;
-    maxCacheTtl = 31536000;
-  };
-
-  xresources.extraConfig = builtins.readFile ./Xresources;
-
-  # Make cursor not tiny on HiDPI screens
-  home.pointerCursor = lib.mkIf (isLinux && !isWSL) {
-    name = "Vanilla-DMZ";
-    package = pkgs.vanilla-dmz;
-    size = 128;
-    x11.enable = true;
-  };
+  # #---------------------------------------------------------------------
+  # # Env vars and dotfiles
+  # #---------------------------------------------------------------------
+  #
+  # home.sessionVariables = {
+  #   LANG = "en_US.UTF-8";
+  #   LC_CTYPE = "en_US.UTF-8";
+  #   LC_ALL = "en_US.UTF-8";
+  #   EDITOR = "nvim";
+  #   PAGER = "less -FirSwX";
+  #   MANPAGER = "${manpager}/bin/manpager";
+  # };
+  #
+  # home.file.".gdbinit".source = ./gdbinit;
+  # home.file.".inputrc".source = ./inputrc;
+  #
+  # xdg.configFile = {
+  #   "i3/config".text = builtins.readFile ./i3;
+  #   "rofi/config.rasi".text = builtins.readFile ./rofi;
+  #
+  # } // (if isDarwin then {
+  #   # Rectangle.app. This has to be imported manually using the app.
+  #   "rectangle/RectangleConfig.json".text = builtins.readFile ./RectangleConfig.json;
+  # } else { }) // (if isLinux then {
+  #   "ghostty/config".text = builtins.readFile ./ghostty.linux;
+  # } else { });
+  #
+  # #---------------------------------------------------------------------
+  # # Programs
+  # #---------------------------------------------------------------------
+  #
+  # programs.gpg.enable = !isDarwin;
+  #
+  # programs.bash = {
+  #   enable = true;
+  #   shellOptions = [ ];
+  #   historyControl = [ "ignoredups" "ignorespace" ];
+  #   initExtra = builtins.readFile ./bashrc;
+  #
+  #   shellAliases = {
+  #     ga = "git add";
+  #     gc = "git commit";
+  #     gco = "git checkout";
+  #     gcp = "git cherry-pick";
+  #     gdiff = "git diff";
+  #     gl = "git prettylog";
+  #     gp = "git push";
+  #     gs = "git status";
+  #     gt = "git tag";
+  #   };
+  # };
+  #
+  # programs.direnv = {
+  #   enable = true;
+  #
+  #   config = {
+  #     whitelist = {
+  #       prefix = [
+  #         "$HOME/code/go/src/github.com/hashicorp"
+  #         "$HOME/code/go/src/github.com/mitchellh"
+  #       ];
+  #
+  #       exact = [ "$HOME/.envrc" ];
+  #     };
+  #   };
+  # };
+  #
+  #
+  # programs.git = {
+  #   enable = true;
+  #   userName = "Mitchell Hashimoto";
+  #   userEmail = "mitchell.hashimoto@gmail.com";
+  #   signing = {
+  #     key = "523D5DC389D273BC";
+  #     signByDefault = true;
+  #   };
+  #   aliases = {
+  #     cleanup = "!git branch --merged | grep  -v '\\*\\|master\\|develop' | xargs -n 1 -r git branch -d";
+  #     prettylog = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(r) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative";
+  #     root = "rev-parse --show-toplevel";
+  #   };
+  #   extraConfig = {
+  #     branch.autosetuprebase = "always";
+  #     color.ui = true;
+  #     core.askPass = ""; # needs to be empty to use terminal for ask pass
+  #     credential.helper = "store"; # want to make this more secure
+  #     github.user = "mitchellh";
+  #     push.default = "tracking";
+  #     init.defaultBranch = "main";
+  #   };
+  # };
+  #
+  # programs.go = {
+  #   enable = true;
+  #   goPath = "code/go";
+  #   goPrivate = [ "github.com/mitchellh" "github.com/hashicorp" "rfc822.mx" ];
+  # };
+  #
+  # programs.tmux = {
+  #   enable = true;
+  #   terminal = "xterm-256color";
+  #   shortcut = "l";
+  #   secureSocket = false;
+  #
+  #   extraConfig = ''
+  #     set -ga terminal-overrides ",*256col*:Tc"
+  #
+  #     set -g @dracula-show-battery false
+  #     set -g @dracula-show-network false
+  #     set -g @dracula-show-weather false
+  #
+  #     bind -n C-k send-keys "clear"\; send-keys "Enter"
+  #
+  #   '';
+  # };
+  #
+  # programs.alacritty = {
+  #   enable = !isWSL;
+  #
+  #   settings = {
+  #     env.TERM = "xterm-256color";
+  #
+  #     key_bindings = [
+  #       { key = "K"; mods = "Command"; chars = "ClearHistory"; }
+  #       { key = "V"; mods = "Command"; action = "Paste"; }
+  #       { key = "C"; mods = "Command"; action = "Copy"; }
+  #       { key = "Key0"; mods = "Command"; action = "ResetFontSize"; }
+  #       { key = "Equals"; mods = "Command"; action = "IncreaseFontSize"; }
+  #       { key = "Subtract"; mods = "Command"; action = "DecreaseFontSize"; }
+  #     ];
+  #   };
+  # };
+  #
+  # programs.kitty = {
+  #   enable = !isWSL;
+  #   extraConfig = builtins.readFile ./kitty;
+  # };
+  #
+  # programs.i3status = {
+  #   enable = isLinux && !isWSL;
+  #
+  #   general = {
+  #     colors = true;
+  #     color_good = "#8C9440";
+  #     color_bad = "#A54242";
+  #     color_degraded = "#DE935F";
+  #   };
+  #
+  #   modules = {
+  #     ipv6.enable = false;
+  #     "wireless _first_".enable = false;
+  #     "battery all".enable = false;
+  #   };
+  # };
+  #
+  # programs.neovim = {
+  #   enable = true;
+  #   package = pkgs.neovim-nightly;
+  #
+  #   withPython3 = true;
+  #
+  #   plugins = with pkgs; [
+  #     customVim.vim-copilot
+  #     customVim.vim-cue
+  #     customVim.vim-fish
+  #     customVim.vim-fugitive
+  #     customVim.vim-glsl
+  #     customVim.vim-misc
+  #     customVim.vim-pgsql
+  #     customVim.vim-tla
+  #     customVim.vim-zig
+  #     customVim.pigeon
+  #     customVim.AfterColors
+  #
+  #     customVim.vim-nord
+  #     customVim.nvim-comment
+  #     customVim.nvim-lspconfig
+  #     customVim.nvim-plenary # required for telescope
+  #     customVim.nvim-telescope
+  #     customVim.nvim-treesitter
+  #     customVim.nvim-treesitter-playground
+  #     customVim.nvim-treesitter-textobjects
+  #
+  #     vimPlugins.vim-airline
+  #     vimPlugins.vim-airline-themes
+  #     vimPlugins.vim-eunuch
+  #     vimPlugins.vim-gitgutter
+  #
+  #     vimPlugins.vim-markdown
+  #     vimPlugins.vim-nix
+  #     vimPlugins.typescript-vim
+  #   ] ++ (lib.optionals (!isWSL) [
+  #     # This is causing a segfaulting while building our installer
+  #     # for WSL so just disable it for now. This is a pretty
+  #     # unimportant plugin anyway.
+  #     customVim.vim-devicons
+  #   ]);
+  # };
+  #
+  # services.gpg-agent = {
+  #   enable = isLinux;
+  #   pinentryFlavor = "tty";
+  #
+  #   # cache the keys forever so we don't get asked for a password
+  #   defaultCacheTtl = 31536000;
+  #   maxCacheTtl = 31536000;
+  # };
+  #
+  # xresources.extraConfig = builtins.readFile ./Xresources;
+  #
+  # # Make cursor not tiny on HiDPI screens
+  # home.pointerCursor = lib.mkIf (isLinux && !isWSL) {
+  #   name = "Vanilla-DMZ";
+  #   package = pkgs.vanilla-dmz;
+  #   size = 128;
+  #   x11.enable = true;
+  # };
 }
