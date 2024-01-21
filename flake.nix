0fd86c11a7b1c321a6292a3395b5ca2678e5eb1c
artsbentley <artsbentley@gmail.com>
2024-01-21 18:31:45 +0100

    change architecutre

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
 
   outputs = { nixpkgs, home-manager, ... }@inputs:
     let
-      system = "x86_64-linux";
+      system = "aarch64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
 
