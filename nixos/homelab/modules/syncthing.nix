18158233d47e19a8962a515d6c5be451111b2deb
artsbentley <artsbentley@gmail.com>
2024-02-11 22:54:57 +0100

    add 0.0.0.0 listen

--- nixos/homelab/modules/syncthing.nix
+++ nixos/homelab/modules/syncthing.nix
@@ -7,7 +7,8 @@
       user = "arar";
       dataDir = "/home/arar/syncthing"; # Default folder for new synced folders
       configDir = "/home/arar/syncthing/.config/syncthing"; # Folder for Syncthing's settings and keys
-      # openDefaultPorts = true;
+      guiAddress = "0.0.0.0:8384";
+      openDefaultPorts = true;
     };
   };
   networking.firewall.allowedTCPPorts = [ 8384 22000 ];
