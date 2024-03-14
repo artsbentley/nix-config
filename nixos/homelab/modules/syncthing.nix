a24290868f973782e18b72c2d94bb2584562e908
artsbentley <artsbentley@gmail.com>
2024-03-14 20:55:57 +0100

    added vaultwarden

--- nixos/homelab/modules/syncthing.nix
+++ nixos/homelab/modules/syncthing.nix
@@ -1,17 +1,20 @@
 { config, pkgs, ... }:
 
 {
-  services = {
-    syncthing = {
-      enable = true;
-      user = "arar";
-      dataDir = "/home/arar/syncthing"; # Default folder for new synced folders
-      configDir = "/home/arar/syncthing/.config/syncthing"; # Folder for Syncthing's settings and keys
-      guiAddress = "0.0.0.0:8384";
-      openDefaultPorts = true;
-    };
+  services.syncthing = {
+    enable = true;
+    user = "arar";
+    dataDir = "/home/arar/syncthing"; # Default folder for new synced folders
+    configDir = "/home/arar/syncthing/.config/syncthing"; # Folder for Syncthing's settings and keys
+    guiAddress = "0.0.0.0:8384";
+    openDefaultPorts = true;
   };
-  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+  networking.firewall.allowedTCPPorts = [ 8384 22000 80 443 ];
   networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 
+
+  services.vaultwarden = {
+    enable = true;
+    dbBackend = "sqlite";
+  };
 }
