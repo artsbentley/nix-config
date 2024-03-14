453f937e716ff01c8b5698c564d647e3f98799dc
artsbentley <artsbentley@gmail.com>
2024-03-14 21:58:52 +0100

    wip

--- nixos/homelab/modules/syncthing.nix
+++ nixos/homelab/modules/syncthing.nix
@@ -18,8 +18,13 @@
     enable = true;
     dbBackend = "sqlite";
     config = {
-      rocketPort = 8000;
-      websocketEnabled = true;
+      # DOMAIN = "https://vault.flake.sh/";
+      # SIGNUPS_ALLOWED = false;
+      # DATABASE_URL = "postgresql://vaultwarden:vaultwarden@192.168.1.211:5432/vaultwarden";
+      LOG_LEVEL = "Info";
+      ROCKET_ADDRESS = "0.0.0.0";
+      ROCKET_PORT = 8222;
+      ROCKET_LOG = "critical";
     };
   };
 }
