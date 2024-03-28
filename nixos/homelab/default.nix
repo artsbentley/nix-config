0d96a331b73e997d6cf1da5ce6dea345f210de89
artsbentley <artsbentley@gmail.com>
2024-03-28 18:57:26 +0100

    wip

--- nixos/homelab/default.nix
+++ nixos/homelab/default.nix
@@ -3,7 +3,7 @@
 
 { inputs, outputs, sops-nix, lib, config, pkgs, ... }: {
   imports = [
-    # ./modules/arr.nix
+    ./modules/arr.nix
     # ./modules/nixarr.nix
     ./modules/syncthing.nix
     # ./modules/network.nix
