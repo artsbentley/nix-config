52d17ed3cf9613d0a6566b1f40fe2318eb6e9582
artsbentley <artsbentley@gmail.com>
2024-07-19 23:14:06 +0300

    wip

--- users/arar/gitconfig.nix
+++ users/arar/gitconfig.nix
@@ -1,9 +1,9 @@
 { inputs, lib, config, pkgs, ... }:
 {
-  age.secrets.gitIncludes = {
-    file = ../../secrets/gitIncludes.age;
-    path = "$HOME/.config/git/includes";
-  };
+  #  age.secrets.gitIncludes = {
+  #    file = ../../secrets/gitIncludes.age;
+  #    path = "$HOME/.config/git/includes";
+  #  };
 
   programs.git = {
     enable = true;
@@ -15,11 +15,11 @@
         sshCommand = "ssh -o 'IdentitiesOnly=yes' -i ~/.ssh/arar";
       };
     };
-    includes = [
-      {
-        path = "~" + (lib.removePrefix "$HOME" config.age.secrets.gitIncludes.path);
-        condition = "gitdir:~/Workspace/Projects/";
-      }
-    ];
+    #    includes = [
+    #      {
+    #        path = "~" + (lib.removePrefix "$HOME" config.age.secrets.gitIncludes.path);
+    #        condition = "gitdir:~/Workspace/Projects/";
+    #      }
+    #    ];
   };
 }
