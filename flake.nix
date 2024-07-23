4fdf5d31dec5b3f3de9bba33bf359821c23ed125
artsbentley <artsbentley@gmail.com>
2024-07-23 21:06:59 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -73,47 +73,6 @@
             vars = import ./machines/nixos/vars.nix;
           };
           modules = [
-            {
-              description = "Your new nix config";
-
-              inputs = {
-                nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-                home-manager = {
-                  url = "github:nix-community/home-manager/release-23.05";
-                  inputs.nixpkgs.follows = "nixpkgs";
-                };
-                hardware.url = "github:nixos/nixos-hardware";
-                # nixarr.url = "github:rasmus-kirk/nixarr";
-              };
-
-              outputs = { nixpkgs, home-manager, ... }@inputs:
-                let
-                  system = "x86_64-linux";
-                  pkgs = nixpkgs.legacyPackages.${system};
-                in
-
-                {
-                  nixosConfigurations = {
-                    arar = nixpkgs.lib.nixosSystem {
-                      specialArgs = { inherit inputs; }; # Pass flake inputs to our config
-                      modules = [
-                        # nixarr.nixosModules.default
-                        ./nixos/homelab/default.nix
-                        ./hardware-configuration.nix
-                      ];
-                    };
-                  };
-
-                  homeConfigurations = {
-                    "arar@nixos" = home-manager.lib.homeManagerConfiguration {
-                      inherit pkgs;
-                      # extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-                      modules = [ ./home-manager/home.nix ];
-                    };
-                  };
-                };
-            }
-
             # Base configuration and modules
             ./modules/podman
             # ./modules/tailscale
@@ -134,17 +93,17 @@
 
             # User-specific configurations
             # ./users/arar
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = false;
-              home-manager.extraSpecialArgs = { inherit inputs; };
-              home-manager.users.arar.imports = [
-                agenix.homeManagerModules.default
-                nix-index-database.hmModules.nix-index
-                ./users/arar/dots.nix
-              ];
-              home-manager.backupFileExtension = "bak";
-            }
+            # home-manager.nixosModules.home-manager
+            # {
+            #   home-manager.useGlobalPkgs = false;
+            #   home-manager.extraSpecialArgs = { inherit inputs; };
+            #   home-manager.users.arar.imports = [
+            #     agenix.homeManagerModules.default
+            #     nix-index-database.hmModules.nix-index
+            #     ./users/arar/dots.nix
+            #   ];
+            #   home-manager.backupFileExtension = "bak";
+            # }
           ];
         };
 
