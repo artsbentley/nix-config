800aca4419451a51bdb3cdd1de54107d1d6b9f85
artsbentley <artsbentley@gmail.com>
2024-07-24 12:34:06 +0200

    wip

--- machines/nixos/default.nix
+++ machines/nixos/default.nix
@@ -71,7 +71,7 @@
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/home/arar/nix-config/flake.nix";
+    flake = "/home/arar/nix-config";
   };
 
 
