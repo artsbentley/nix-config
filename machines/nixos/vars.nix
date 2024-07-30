a6ec535284446d5143f30579a0cabb0b3f2c1581
artsbentley <artsbentley@gmail.com>
2024-07-30 21:23:29 +0200

    wip

--- machines/nixos/vars.nix
+++ machines/nixos/vars.nix
@@ -2,7 +2,7 @@
   domainName = "arar.dev";
   timeZone = "Europe/Berlin";
   nasMount = "/home/arar/nas";
-  serviceConfigRoot = "/persist/opt/services";
+  serviceConfigRoot = "/home/arar/nas/persist";
   # slowArray = "/mnt/mergerfs_slow";
   # cacheArray = "/mnt/cache";
 }
