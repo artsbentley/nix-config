babd835d3fe598c90b03bb47063553d03687e908
artsbentley <artsbentley@gmail.com>
2024-08-12 16:16:33 +0200

    wip

--- containers/vaultwarden/default.nix
+++ containers/vaultwarden/default.nix
@@ -14,13 +14,28 @@ in
         ports = [ "8081:80" ];
         extraOptions = [ ];
         volumes = [ "${vars.serviceConfigRoot}/vaultwarden:/data" ];
-        environmentFiles = [
-          config.age.secrets.vaultwarden.path
-        ];
+        environmentFiles = [ config.age.secrets.vaultwarden.path ];
         environment = {
           # DOMAIN = "https://pass.${vars.domainName}";
           WEBSOCKET_ENABLED = "true";
         };
+      };
+    };
+    # TODO this needs a cronjob to be run daily
+    bw-export = {
+      image = "0netx/bw-export";
+      # autoStart = true;
+      volumes = [
+        "${vars.nasMount}/Backup/vaultwarden/arar/data:/var/attachment"
+        "${vars.nasMount}/Backup/vaultwarden/arar/attachment:/var/attachment"
+      ];
+      # NOTE: this .env might collide with vaultwarden, if it does, seperate out into
+      # its own env file
+      environmentFiles = [ config.age.secrets.vaultwarden.path ];
+      environment = {
+        PUID = "${toString config.users.users.share.uid}";
+        PGID = "${toString config.users.groups.share.gid}";
+        KEEP_LAST_BACKUPS = 50;
       };
     };
   };
