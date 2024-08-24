a0a02b8bef835bd08e197429429fa61eacf0fad6
artsbentley <artsbentley@gmail.com>
2024-08-25 01:46:17 +0200

    wip

--- containers/paperless/default.nix
+++ containers/paperless/default.nix
@@ -53,7 +53,7 @@ in
       paperless = {
         image = "ghcr.io/paperless-ngx/paperless-ngx";
         autoStart = true;
-        ports = [ "8010:8010" ];
+        ports = [ "8000:8010" ];
         extraOptions = [
           "--device=/dev/dri:/dev/dri"
         ];
