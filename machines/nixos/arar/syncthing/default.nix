07fb41de7235f910761ddb3ba70d1ca8a08b073b
artsbentley <artsbentley@gmail.com>
2024-08-25 01:41:04 +0200

    wip

--- machines/nixos/arar/syncthing/default.nix
+++ machines/nixos/arar/syncthing/default.nix
@@ -5,22 +5,18 @@ let
   ];
 in
 {
-  systemd.tmpfiles.rules = map (x: "d ${x} 0775 share share - -") directories;
-  networking.firewall = {
-    allowedTCPPorts = [ 8384 22000 ];
-    allowedUDPPorts = [ 22000 21027 ];
-  };
   services = {
     syncthing = {
       enable = true;
-      user = "share";
-      group = "share";
+      user = "arar";
+      dataDir = "/home/arar/syncthing"; # Default folder for new synced folders
+      configDir = "/home/arar/syncthing/.config/syncthing"; # Folder for Syncthing's settings and keys
       guiAddress = "0.0.0.0:8384";
-      overrideFolders = false;
-      overrideDevices = false;
-      dataDir = "${vars.nasMount}/Syncthing";
-      configDir = "${vars.serviceConfigRoot}/syncthing";
+      openDefaultPorts = true;
     };
   };
+  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+
 }
 
