57cfee2e4d5787cc74ae545ab653e8aeeef032b1
artsbentley <artsbentley@gmail.com>
2024-08-24 21:31:42 +0200

    wip

--- containers/backrest/default.nix
+++ containers/backrest/default.nix
@@ -1,4 +1,5 @@
 { config, vars, ... }:
+
 let
   directories = [
     "${vars.serviceConfigRoot}/backrest"
@@ -10,25 +11,51 @@ in
   virtualisation.oci-containers = {
     containers = {
       backrest = {
-          image = "garethgeorge/backrest";
-          container_name = "backrest";
-          autoStart = true;
-          ports = [ "9898:9898" ];
-          volumes = [
-            "${vars.serviceConfigRoot}/backrest:/config"
-            "${vars.nasMount}/Backups/restic:data"
-            # Uncomment and adjust these as needed
-            # "${server.data}/backrest:/data"
-            # "${server.cache}/backrest:/cache"
-            # "${server.data}:/userdata"
-          ];
-          environment = {
-            BACKREST_CONFIG = "/config/config.json";
-            BACKREST_DATA = "/data";
-            XDG_CACHE_HOME = "/cache";
-          };
+        image = "garethgeorge/backrest:latest";
+        autoStart = true;
+        ports = [ "9898:9898" ];
+        volumes = [
+          "${vars.serviceConfigRoot}/backrest:/config"
+          "${vars.nasMount}/Backups/restic:data"
+          # Uncomment and adjust these as needed
+          # "${server.data}/backrest:/data"
+          # "${server.cache}/backrest:/cache"
+          # "${server.data}:/userdata"
+        ];
+        environment = {
+          BACKREST_CONFIG = "/config/config.json";
+          BACKREST_DATA = "/data";
+          XDG_CACHE_HOME = "/cache";
+        };
       };
     };
   };
 }
 
+
+
+#   virtualisation.oci-containers = {
+#     containers = {
+#       backrest = {
+#           image = "garethgeorge/backrest";
+#           container_name = "backrest";
+#           autoStart = true;
+#           ports = [ "9898:9898" ];
+#           volumes = [
+#             "${vars.serviceConfigRoot}/backrest:/config"
+#             "${vars.nasMount}/Backups/restic:data"
+#             # Uncomment and adjust these as needed
+#             # "${server.data}/backrest:/data"
+#             # "${server.cache}/backrest:/cache"
+#             # "${server.data}:/userdata"
+#           ];
+#           environment = {
+#             BACKREST_CONFIG = "/config/config.json";
+#             BACKREST_DATA = "/data";
+#             XDG_CACHE_HOME = "/cache";
+#           };
+#       };
+#     };
+#   };
+# }
+#
