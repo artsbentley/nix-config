92e62ee7f76a650d7148344af94f70fc36ec14ae
artsbentley <artsbentley@gmail.com>
2024-08-25 11:33:08 +0200

    wip

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 - [ ] create and mount all accessible NAS mount (for backups)
 - [ ] setup restic
 - [ ] setup backrest
+- [ ] setup vaultwarden backup + syncthing to devices
 
 TODO: seperate nixos into modules, see here:
 https://github.com/LongerHV/nixos-configuration/blob/f844fab9280eeefcf689c68b1f630cd42fa3cc8a/modules/nixos/homelab/default.nix
--- machines/nixos/arar/syncthing/default.nix
+++ machines/nixos/arar/syncthing/default.nix
@@ -4,6 +4,8 @@ let
     "${vars.serviceConfigRoot}/syncthing"
   ];
 in
+# TODO: decide if "share" user makes sense here or is "arar" would be better.
+  # how would the interaction work for devices that arent the general server?
 {
   systemd.tmpfiles.rules = map (x: "d ${x} 0775 share share - -") directories;
   networking.firewall = {
@@ -16,10 +18,20 @@ in
       user = "arar";
       # group = "share";
       guiAddress = "0.0.0.0:8384";
-      overrideFolders = false;
+      overrideFolders = true;
       overrideDevices = false;
       dataDir = "${vars.nasMount}/Syncthing";
       configDir = "${vars.serviceConfigRoot}/syncthing";
+      settings = {
+        folders = {
+          "Downloads" = {
+            path = "${vars.nasMount}/Syncthing/Downloads";
+          };
+          "root-syncthing" = {
+            path = "${vars.nasMount}/Syncthing";
+          };
+        };
+      };
     };
   };
 }
