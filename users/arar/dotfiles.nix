39769263cd2ec565b3dad9f6cb23f1abd2c22b80
artsbentley <artsbentley@gmail.com>
2024-08-26 23:28:46 +0200

    wip

--- users/arar/dotfiles.nix
+++ users/arar/dotfiles.nix
@@ -13,9 +13,10 @@
   home.homeDirectory = "/home/arar";
   home.stateVersion = "23.11";
   #
+  # TODO: wrap dotfiles directory location in variable
   # TODO: put dotfiles in nix repo and use relative path below 
   home.file = {
-    ".config/zsh".source = "../..dotfiles/zsh";
+    ".config/zsh".source = "../../dotfiles/zsh";
     # ".config/wezterm".source = ~/dotfiles/wezterm;
     # ".config/skhd".source = ~/dotfiles/skhd;
     # ".config/starship".source = ~/dotfiles/starship;
