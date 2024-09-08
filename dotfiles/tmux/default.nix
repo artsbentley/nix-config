4572af1c874f68b724ea4b08cb0f8457ddae338e
artsbentley <artsbentley@gmail.com>
2024-09-08 11:58:52 +0200

    wip

--- dotfiles/tmux/default.nix
+++ dotfiles/tmux/default.nix
@@ -8,10 +8,20 @@
   };
   programs.tmux = {
     enable = true;
+    aggressiveResize = true;
+    clock24 = true;
+    escapeTime = 0;
+    historyLimit = 1000000;
     sensibleOnTop = false;
+    terminal = "tmux-256color";
+    keyMode = "vi";
+    baseIndex = 1;
+    extraConfig = lib.strings.fileContents ./tmux.conf;
+
     # TODO: 
     # proper home dir
-    extraConfig = ''source-file ~/.config/tmux/tmuxconfig.conf'';
+    # extraConfig = ''source-file ~/.config/tmux/tmuxconfig.conf'';
+
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
       fzf-tmux-url
