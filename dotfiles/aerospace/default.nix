2ce1cb45123300d1502a89820bf7e2db74d09876
artsbentley <artsbentley@gmail.com>
2024-09-08 21:40:04 +0200

    wip

--- dotfiles/aerospace/default.nix
+++ dotfiles/aerospace/default.nix
@@ -1,16 +1,6 @@
 { inputs, lib, config, pkgs, ... }:
 {
-  # home.packages = with pkgs; [
-  #   wezterm
-  # ];
-  # programs.wezterm = {
-  #   enable = true;
-  #   enableZshIntegration = true;
-  # };
   home.file = {
     ".config/aerospace/aerospace.toml".source = ./aerospace.toml;
   };
-  homebrew.casks = [
-    "nikitabobko/tap/aerospace"
-  ];
 }
