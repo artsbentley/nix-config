ee9cb91757e2da7cd28315f661d20088924fbf18
artsbentley <artsbentley@gmail.com>
2024-09-09 22:30:02 +0200

    wip

--- dotfiles/scripts/default.nix
+++ dotfiles/scripts/default.nix
@@ -1,9 +1,31 @@
 { inputs, lib, config, pkgs, ... }:
 {
   home.file = {
-    ".local/bin" = {
-      source = ./bin;
-      recursive = true;
-    };
+    ".config/scripts".source = ./scripts;
   };
+
+  programs.zsh.initExtra = ''
+            path=("$HOME/.config/scripts" $path)
+    		'';
 }
+
+# {
+#   home.file = {
+#     ".local/bin" = {
+#       source = ./bin;
+#       recursive = true;
+#     };
+#   };
+# let
+#   binPath = "${config.home.homeDirectory}/.local/bin";
+#   scriptsPath = "${config.home.homeDirectory}/dotfiles/modules/home/scripts";
+# in
+# {
+#   home.file."${binPath}".source =
+#     config.lib.file.mkOutOfStoreSymlink "${scriptsPath}";
+#
+#   programs.zsh.initExtra = ''
+#     if [ -d "$HOME/.local/bin" ]; then
+#       export PATH="${binPath}:$PATH"
+#     fi
+#   '';
