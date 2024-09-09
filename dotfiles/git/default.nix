551d4255bd0d0fb5634cd9eedcd44441124122c7
artsbentley <artsbentley@gmail.com>
2024-09-09 23:21:27 +0200

    wip

--- dotfiles/git/default.nix
+++ dotfiles/git/default.nix
@@ -11,14 +11,32 @@
     userEmail = "artsbentley@gmail.com";
     delta = {
       enable = true;
+      # options = {
+      #   navigate = true;
+      #   line-numbers = true;
+      #   # syntax-theme = "gruvbox-dark";
+      #   syntax-theme = config.programs.bat.config.theme;
+      #   dark = true;
+      #   side-by-side = true;
+      #   hyperlinks = true;
+      # };
       options = {
-        navigate = true;
-        line-numbers = true;
-        # syntax-theme = "gruvbox-dark";
+        features = "hyperlinks";
+        file-added-label = "[+]";
+        file-copied-label = "[C]";
+        file-decoration-style = "yellow ul";
+        file-modified-label = "[M]";
+        file-removed-label = "[-]";
+        file-renamed-label = "[R]";
+        file-style = "yellow bold";
+        hunk-header-decoration-style = "omit";
+        hunk-header-style = "syntax italic #303030";
+        minus-emph-style = "syntax bold #780000";
+        minus-style = "syntax #400000";
+        plus-emph-style = "syntax bold #007800";
+        plus-style = "syntax #004000";
         syntax-theme = config.programs.bat.config.theme;
-        dark = true;
-        side-by-side = true;
-        hyperlinks = true;
+        width = 1;
       };
     };
     aliases = {
