bcdb4f9112bcacb7d3b1307b1f57ebaf3750c716
artsbentley <artsbentley@gmail.com>
2024-09-14 23:59:29 +0200

    wip

--- dotfiles/tmux/default.nix
+++ dotfiles/tmux/default.nix
@@ -14,6 +14,7 @@
     keyMode = "vi";
     baseIndex = 1;
     extraConfig = lib.strings.fileContents ./tmux.conf;
+    shell = "${pkgs.zsh}/bin/zsh";
 
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
