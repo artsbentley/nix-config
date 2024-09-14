08a22be3982c3c373b34cecb49a04c5f478b5166
artsbentley <artsbentley@gmail.com>
2024-09-14 23:05:53 +0200

    wip

--- dotfiles/lazygit/default.nix
+++ dotfiles/lazygit/default.nix
@@ -1,12 +1,12 @@
 { inputs, lib, config, pkgs, ... }:
 {
-  # home.packages = with pkgs; [
-  #   lazygit
-  # ];
-  # #
-  # programs.lazygit = {
-  #   enable = true;
-  # };
+  home.packages = with pkgs; [
+    lazygit
+  ];
+  #
+  programs.lazygit = {
+    enable = true;
+  };
   home.file = {
     ".config/lazygit/config.yml".source = ./lazygit/config.yml;
   };
--- dotfiles/zsh/default.nix
+++ dotfiles/zsh/default.nix
@@ -74,6 +74,7 @@
         home = "cd ~";
         c = "clear";
         ":Yazi" = "ya";
+        XDG_CONFIG_HOME = "$HOME/.config";
         stopdocker = "sudo systemctl stop --all 'podman-*' && podman builder prune -f -a && podman network prune -f && podman image prune -a -f && podman container prune -f";
         startdocker = "sudo systemctl start --all 'podman-*'";
         # update = "cd ~/nix-config && git pull && sudo nixos-rebuild switch --flake .#arar && cd -";
