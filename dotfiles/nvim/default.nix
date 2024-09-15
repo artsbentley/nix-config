97764d1316929b6bef2739a663de6d2dd2284445
artsbentley <artsbentley@gmail.com>
2024-09-15 18:45:22 +0200

    wip

--- dotfiles/nvim/default.nix
+++ dotfiles/nvim/default.nix
@@ -10,7 +10,6 @@
 
   home.activation = {
     dotfileSetup = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      # Detect the OS (Darwin or Linux)
       os=$(uname)
       if [ "$os" = "Darwin" ]; then
         home_dir="/Users/$(whoami)"
@@ -18,13 +17,9 @@
         home_dir="/home/$(whoami)"
       fi
 
-      # Change to dotfiles directory
-      pushd $home_dir/nix-conf/dotfiles
-
-      # Symlink only 'nvim' into the .config directory
+      pushd $home_dir/nix-config/dotfiles
       ${pkgs.stow}/bin/stow -vt $home_dir/.config nvim
 
-      # Return to the previous directory
       popd
     '';
   };
