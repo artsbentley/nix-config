ade3d1986a244fc1eb8e66f43af9d9661435e049
artsbentley <artsbentley@gmail.com>
2024-09-15 18:24:38 +0200

    wip

--- dotfiles/nvim/default.nix
+++ dotfiles/nvim/default.nix
@@ -1,8 +1,34 @@
 { inputs, lib, config, pkgs, ... }:
 {
-  home.file = {
-    ".config/nvim".source = ./nvim;
+  home.packages = with pkgs; [
+    stow
+  ];
+
+  # home.file = {
+  #   ".config/nvim".source = ./nvim;
+  # };
+
+  home.activation = {
+    dotfileSetup = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      # Detect the OS (Darwin or Linux)
+      os=$(uname)
+      if [ "$os" = "Darwin" ]; then
+        home_dir="/Users/arar"
+      else
+        home_dir="/home/arar"
+      fi
+
+      # Change to dotfiles directory
+      pushd $(home_dir)/nix-config/dotfiles
+
+      # Symlink only 'nvim' into the .config directory
+      ${pkgs.stow}/bin/stow -vt $(home_dir)/.config nvim
+
+      # Return to the previous directory
+      popd
+    '';
   };
+
 }
 
 
