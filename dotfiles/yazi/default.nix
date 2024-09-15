b04c7f0c6910418bbfe59a4873e501406ce6f4c6
artsbentley <artsbentley@gmail.com>
2024-09-15 17:33:22 +0200

    test mkoutofstoresymlink

--- dotfiles/yazi/default.nix
+++ dotfiles/yazi/default.nix
@@ -15,7 +15,10 @@
     enableZshIntegration = true;
   };
 
+  # home.file = {
+  #   ".config/yazi".source = ./yazi;
+  # };
   home.file = {
-    ".config/yazi".source = ./yazi;
+    ".config/yazi".source = config.lib.file.mkOutOfStoreSymlink "./yazi";
   };
 }
