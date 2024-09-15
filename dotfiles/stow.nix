b5b5dcbac6c2e26aca2f1974654d5e245aae693d
artsbentley <artsbentley@gmail.com>
2024-09-15 22:53:35 +0200

    wip

--- dotfiles/stow.nix
+++ dotfiles/stow.nix
@@ -4,18 +4,19 @@
     stow
   ];
 
+  # see https://github.com/arminveres/nix-conf/blob/3185d4f86b9ea13da518a9c6112115340cfdbdc1/modules/home/default.nix#L109
   home.activation = {
     dotfileSetup = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      os=$(uname)
-      if [ "$os" = "Darwin" ]; then
-        home_dir="/Users/$(whoami)"
-      else
-        home_dir="/home/$(whoami)"
-      fi
-
-      pushd $home_dir/nix-config/dotfiles
-      ${pkgs.stow}/bin/stow -vt $home_dir/.config nvim scripts yazi
-      popd
+            # os=$(uname)
+            # if [ "$os" = "Darwin" ]; then
+            #   home_dir="/Users/$(whoami)"
+            # else
+            #   home_dir="/home/$(whoami)"
+            # fi
+      	  home_dir="$HOME/$(whoami)"
+            pushd $home_dir/nix-config/dotfiles
+            ${pkgs.stow}/bin/stow -vt $home_dir/.config nvim scripts yazi
+            popd
     '';
   };
 }
