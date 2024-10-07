15027d04187b45721318313a987ea5ec2a0365cf
artsbentley <artsbentley@gmail.com>
2024-10-07 21:04:39 +0200

    wip

--- dotfiles/zsh/default.nix
+++ dotfiles/zsh/default.nix
@@ -77,6 +77,9 @@
         home = "cd ~";
         c = "clear";
         ":Yazi" = "ya";
+
+        gpl = "git pull --rebase --autostash";
+
         stopdocker = "sudo systemctl stop --all 'podman-*' && podman builder prune -f -a && podman network prune -f && podman image prune -a -f && podman container prune -f";
         startdocker = "sudo systemctl start --all 'podman-*'";
         # update = "cd ~/nix-config && git pull && sudo nixos-rebuild switch --flake .#arar && cd -";
