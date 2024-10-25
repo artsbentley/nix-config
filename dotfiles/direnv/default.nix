9ad8a23ba42b2c14b7d8af8523bbbbb0943c4894
artsbentley <artsbentley@gmail.com>
2024-10-25 21:29:28 +0200

    wip

--- dotfiles/direnv/default.nix
+++ dotfiles/direnv/default.nix
@@ -1,6 +1,9 @@
 { inputs, lib, config, pkgs, ... }:
 {
   home.file = {
-    ".config/direnv".source = ./direnv;
+    ".config/direnv".source = .config/direnv;
+    # ".config/direnv/direnvrc".source = ./direnv;
+    # ".config/direnv/direnv.toml".source = ./direnv/;
+
   };
 }
