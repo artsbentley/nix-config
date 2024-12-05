52f81b86740060930ec845242d22585b107d33eb
artsbentley <artsbentley@gmail.com>
2024-12-05 18:40:04 +0100

    wipe

--- machines/surface-nixos/default.nix
+++ machines/surface-nixos/default.nix
@@ -62,9 +62,8 @@
 
   nix.settings.experimental-features = lib.mkDefault [ "nix-command" "flakes" ];
 
-  # services.automatic-timezoned.enable = true;
+  services.automatic-timezoned.enable = true;
   services.localtimed.enable = true;
-  time.timeZone = "Europe/Paris";
 
   programs.git.enable = true;
   programs.mosh.enable = true;
