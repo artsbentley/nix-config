4102f0c5afb124b83b1c7f21fa07956081d8cab9
artsbentley <artsbentley@gmail.com>
2024-12-24 16:30:57 +0100

    hyprland wip

--- machines/surface-nixos/default.nix
+++ machines/surface-nixos/default.nix
@@ -114,15 +114,18 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  # WARNING:
-  services.xserver.windowManager.i3.enable = true;
-  services.xserver.displayManager.sessionPackages = [ pkgs.i3 pkgs.xsession ];
 
   # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome = {
-  #   enable = true;
-  # };
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome = {
+    enable = true;
+  };
+
+  # Hyprland
+  programs.hyprland.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
 
 
   services.xserver.xkb = {
@@ -146,8 +149,8 @@
   };
 
 
-  services.xserver.displayManager.autoLogin.enable = true;
-  services.xserver.displayManager.autoLogin.user = "arar";
+  # services.xserver.displayManager.autoLogin.enable = true;
+  # services.xserver.displayManager.autoLogin.user = "arar";
 
   programs.firefox.enable = true;
 
@@ -187,12 +190,13 @@
     sesh
     uv
 
-    # WARNING: disable i3 for now
-    i3
-    i3status
-    i3lock
-    dmenu
-    xorg.xinit
+    # Hyprland
+    waybar
+    dunst
+    hyprpaper
+    rofi-wayland
+    networkmanagerapplet
+
 
     # USB harddrive
     usbutils
