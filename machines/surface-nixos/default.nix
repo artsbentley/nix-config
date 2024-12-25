c2099225227e9be2786eeb792e9a9fb63764e17c
artsbentley <artsbentley@gmail.com>
2024-12-25 16:13:50 +0100

    stylix

--- machines/surface-nixos/default.nix
+++ machines/surface-nixos/default.nix
@@ -172,6 +172,7 @@
   # inputs.stylix.enable = true;
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+  stylix.image = ./wallpaper.jpg;
 
 
 
