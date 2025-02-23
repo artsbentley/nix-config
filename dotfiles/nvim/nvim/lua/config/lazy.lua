2274659aad4edecc3517164a9d58c6ccf7931f6c
artsbentley <artsbentley@gmail.com>
2025-02-23 23:36:53 +0100

    telescope to snacks

--- dotfiles/nvim/nvim/lua/config/lazy.lua
+++ dotfiles/nvim/nvim/lua/config/lazy.lua
@@ -13,7 +13,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 -- vim.cmd([[command! -nargs=0 GoToFile :Telescope find_files hidden=true]])
 -- TODO: add cwd to picker
 vim.cmd('command! -nargs=0 GoToFileHere lua Snacks.picker.files({finder="files", format="file", show_empty=true, supports_live=true, cwd=vim.fn.expand("%:p:h")})')
-vim.cmd([[command! -nargs=0 GoToGitFile :Telescope git_files hidden=true]])
+vim.cmd("command! -nargs=0 GoToGitFile lua Snacks.picker.git_files({hidden = true})")
 vim.cmd([[command! -nargs=0 TelescopeBuffers :lua require("telescope.builtin").buffers({ layout_strategy='vertical', layout_config={width=0.3, height=0.8}, sort_lastused = true })]])
 
 --  Telekasten
