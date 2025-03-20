5f733da2521f4468372eacd722dacf164f5cb988
artsbentley <artsbentley@gmail.com>
2025-03-20 22:20:01 +0100

    claenup autocmd

--- dotfiles/nvim/nvim/lua/config/autocmds.lua
+++ dotfiles/nvim/nvim/lua/config/autocmds.lua
@@ -2,11 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = { "*tmux.conf" },
-    command = "execute 'silent !tmux source <afile> --silent'",
-})
-
+-- no neck pain
 vim.api.nvim_create_autocmd("VimEnter", {
     callback = function()
         vim.cmd("NoNeckPain")
@@ -34,6 +30,7 @@ vim.api.nvim_create_autocmd("VimEnter", {
 --     end,
 -- })
 
+-- markdown settings
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
     pattern = { "*.mdx", "*.md" },
     callback = function()
@@ -41,6 +38,7 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
     end,
 })
 
+-- proper indentation
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
     pattern = { "*.ex", "*.gleam", "*.nix", "*.tsx" },
     callback = function()
@@ -59,29 +57,39 @@ vim.api.nvim_create_autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
     end,
 })
 
+-- -- execute query on save
+-- local dadbod_group = vim.api.nvim_create_augroup("DadbodExecuteOnSave", { clear = true })
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--     pattern = "*/dadbod_ui/tmp/*",
+--     group = dadbod_group,
+--     callback = function()
+--         vim.cmd("DBUIExecuteQuery")
+--     end,
+-- })
+
 -- script to close all buffers that havent been "touched" source: https://www.reddit.com/r/neovim/comments/12c4ad8/closing_unused_buffers/
-local id = vim.api.nvim_create_augroup("startup", {
-    clear = false,
-})
-
-local persistbuffer = function(bufnr)
-    bufnr = bufnr or vim.api.nvim_get_current_buf()
-    vim.fn.setbufvar(bufnr, "bufpersist", 1)
-end
-
-vim.api.nvim_create_autocmd({ "BufRead" }, {
-    group = id,
-    pattern = { "*" },
-    callback = function()
-        vim.api.nvim_create_autocmd({ "InsertEnter", "BufModifiedSet" }, {
-            buffer = 0,
-            once = true,
-            callback = function()
-                persistbuffer()
-            end,
-        })
-    end,
-})
+-- local id = vim.api.nvim_create_augroup("startup", {
+--     clear = false,
+-- })
+--
+-- local persistbuffer = function(bufnr)
+--     bufnr = bufnr or vim.api.nvim_get_current_buf()
+--     vim.fn.setbufvar(bufnr, "bufpersist", 1)
+-- end
+--
+-- vim.api.nvim_create_autocmd({ "BufRead" }, {
+--     group = id,
+--     pattern = { "*" },
+--     callback = function()
+--         vim.api.nvim_create_autocmd({ "InsertEnter", "BufModifiedSet" }, {
+--             buffer = 0,
+--             once = true,
+--             callback = function()
+--                 persistbuffer()
+--             end,
+--         })
+--     end,
+-- })
 
 -- vim.keymap.set("n", "<Leader>bc", function()
 --     local curbufnr = vim.api.nvim_get_current_buf()
