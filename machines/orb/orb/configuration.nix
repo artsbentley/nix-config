5f3560eed1d0dbaaa0f57e4bad2d6751b31b3ab6
artsbentley <artsbentley@gmail.com>
2025-04-17 18:29:21 +0200

    remove stateversinOn

--- machines/orb/orb/configuration.nix
+++ machines/orb/orb/configuration.nix
@@ -74,14 +74,6 @@ with lib;
     EDITOR = "nvim";
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.05"; # Did you read the comment?
-
   # As this is intended as a stadalone image, undo some of the minimal profile stuff
   documentation.enable = true;
   documentation.nixos.enable = true;
