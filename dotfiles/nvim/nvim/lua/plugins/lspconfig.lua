770edc3b6c18ed88ce8fd1ea56b413ed061c93b3
artsbentley <artsbentley@gmail.com>
2025-04-21 14:22:14 +0200

    chore: fix lspformat config

--- dotfiles/nvim/nvim/lua/plugins/lspconfig.lua
+++ dotfiles/nvim/nvim/lua/plugins/lspconfig.lua
@@ -1,54 +1,32 @@
 return {
-    "neovim/nvim-lspconfig",
-    opts = {
-        inlay_hints = { enabled = false },
-        servers = {
-            pyright = {
-                settings = {
-                    pyright = {
-                        disableOrganizeImports = true, -- Using Ruff
-                    },
-                    python = {
-                        analysis = {
-                            ignore = { "*" }, -- Using Ruff
-                            typeCheckingMode = "off", -- Using mypy
-                        },
-                    },
-                },
-            },
-            setup = {
-                ruff_lsp = function()
-                    require("lazyvim.util").lsp.on_attach(function(client, _)
-                        if client.name == "ruff_lsp" then
-                            -- Disable hover in favor of Pyright
-                            client.server_capabilities.hoverProvider = false
-                        end
-                    end)
-                end,
-            },
-
-            -- pyright = {
-            --     mason = false,
-            --     autostart = false,
-            -- },
-            -- pyright = {
-            --     capabilities = (function()
-            --         local capabilities = vim.lsp.protocol.make_client_capabilities()
-            --         capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
-            --         return capabilities
-            --     end)(),
-            --     settings = {
-            --         python = {
-            --             analysis = {
-            --                 useLibraryCodeForTypes = true,
-            --                 diagnosticSeverityOverrides = {
-            --                     reportUnusedVariable = "warning", -- or anything
-            --                 },
-            --                 typeCheckingMode = "basic",
-            --             },
-            --         },
-            --     },
-            -- },
-        },
-    },
+	"neovim/nvim-lspconfig",
+	opts = {
+		inlay_hints = { enabled = false },
+		servers = {
+			pyright = {
+				settings = {
+					pyright = {
+						disableOrganizeImports = true,
+					},
+					python = {
+						analysis = {
+							ignore = { "*" },
+							typeCheckingMode = "off",
+						},
+					},
+				},
+			},
+			ruff_lsp = {}, -- this tells LazyVim to load this server
+		},
+		setup = {
+			ruff_lsp = function()
+				require("lazyvim.util").lsp.on_attach(function(client, _)
+					if client.name == "ruff_lsp" then
+						client.server_capabilities.hoverProvider = false
+					end
+				end)
+			end,
+		},
+	},
 }
+
