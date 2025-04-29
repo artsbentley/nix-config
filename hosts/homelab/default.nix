380496ef89a8ef1ac6fca81078b4befc5cabd379
artsbentley <artsbentley@gmail.com>
2025-04-29 13:03:17 +0200

    add inter common hardware

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -7,7 +7,7 @@
 , ...
 }: {
   imports = [
-    # inputs.nixos-hardware.nixosModules.common-cpu-amd
+    inputs.nixos-hardware.nixosModules.common-cpu-intel
     # inputs.nixos-hardware.nixosModules.common-gpu-amd
     # inputs.nixos-hardware.nixosModules.common-pc-ssd
     ./hardware-configuration.nix
