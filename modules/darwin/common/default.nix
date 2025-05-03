2bae456dd3d678fe628439382ddb11d6bf5d1093
artsbentley <artsbentley@gmail.com>
2025-05-03 13:56:36 +0200

    wip

--- modules/darwin/common/default.nix
+++ modules/darwin/common/default.nix
@@ -30,7 +30,8 @@
   #  # STYLIX
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-  stylix.autoEnable = false;
+  # stylix.autoEnable = false;
+  stylix.targets.wezterm.enable = false;
 
 
   # Nix settings
