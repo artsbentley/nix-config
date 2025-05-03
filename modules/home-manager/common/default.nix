db6b4bfbe5bef34059d376d953e2649a9ac371dc
artsbentley <artsbentley@gmail.com>
2025-05-03 13:09:04 +0200

    wip

--- modules/home-manager/common/default.nix
+++ modules/home-manager/common/default.nix
@@ -40,7 +40,7 @@ in
   stylix.targets = {
     tmux.enable = false;
     yazi.enable = false;
-    wezterm.enable = false;
+    wezterm.enable = true;
 
     # waybar.enable = true;
     # firefox.enable = true;
