f11cf35e814dd4ee36530977c8ba82f7b8937343
artsbentley <artsbentley@gmail.com>
2025-05-03 15:15:06 +0200

    wip

--- modules/darwin/common/default.nix
+++ modules/darwin/common/default.nix
@@ -31,7 +31,7 @@
   stylix.enable = true;
   # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
   stylix.base16Scheme = ../../../theme/gruvbox.yml;
-  stylix.autoEnable = false;
+  # stylix.autoEnable = false;
 
 
   # Nix settings
