bd28b19a4fe0f6fcab87d6cc98adf5c31465e22d
artsbentley <artsbentley@gmail.com>
2025-05-04 20:37:59 +0200

    wip

--- modules/nixos/homelab/miniflux/default.nix
+++ modules/nixos/homelab/miniflux/default.nix
@@ -7,7 +7,9 @@
     enable = true;
     adminCredentialsFile = config.age.secrets.minifluxAdminPassword.path;
     config = {
-      LISTEN_ADDR = "127.0.0.1:8060";
+      CREATE_ADMIN = "1";
+      LISTEN_ADDR = "127.0.0.1:8067";
+      DISABLE_LOCAL_AUTH = "true";
     };
   };
   networking.firewall.allowedTCPPorts = [ 8060 ];
