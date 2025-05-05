b8ef701401c4adc5c9600ae34a9dc98ca1ebbf39
artsbentley <artsbentley@gmail.com>
2025-05-05 18:41:15 +0200

    clean bat

--- modules/home-manager/dotfiles/bat/default.nix
+++ modules/home-manager/dotfiles/bat/default.nix
@@ -3,9 +3,7 @@
   programs.bat = {
     enable = true;
     config = {
-      # TODO: theme not working currently
       theme = lib.mkIf (!hostConfig.enableStylix) "gruvbox-dark";
-      # theme = if !pkgs.stdenv.isLinux then "gruvbox-dark" else null;
       italic-text = "always";
     };
   };
