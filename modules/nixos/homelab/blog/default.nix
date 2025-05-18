b9249c49e63915aeb9d418c85a754a454c9c8547
artsbentley <artsbentley@gmail.com>
2025-05-18 19:04:35 +0200

    wip

--- modules/nixos/homelab/blog/default.nix
+++ modules/nixos/homelab/blog/default.nix
@@ -5,16 +5,6 @@ let
   publicDir = "${siteDir}/public";
 in
 {
-  # systemd.services.zola-build = {
-  #   description = "Zola static site build";
-  #   wantedBy = [ "multi-user.target" ];
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     WorkingDirectory = siteDir;
-  #     ExecStart = "${pkgs.zola}/bin/zola build";
-  #   };
-  # };
-
   systemd.services.zola-server = {
     description = "Serve Zola site with miniserve";
     wantedBy = [ "multi-user.target" ];
@@ -22,14 +12,18 @@ in
       ExecStartPre = "${pkgs.zola}/bin/zola build";
       ExecStart = "${pkgs.miniserve}/bin/miniserve ${publicDir} --index index.html --port 1111 --interfaces 0.0.0.0";
       Restart = "always";
-      Type = "oneshot";
       WorkingDirectory = siteDir;
     };
-    path = [ pkgs.nix ];
-    startAt = "*:0/5";
+  };
+
+  system.activationScripts.zolaRebuild = {
+    text = ''
+      echo -e "\e[32mRebuilding Zola site and restarting server...\e[0m"
+      ${pkgs.systemd}/bin/systemctl try-restart zola-server.service || true
+    '';
+    deps = [ "users" "groups" ];
   };
 
   networking.firewall.allowedTCPPorts = [ 1111 ];
   environment.systemPackages = with pkgs; [ zola miniserve ];
 }
-
