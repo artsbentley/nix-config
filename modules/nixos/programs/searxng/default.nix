dec6f36653d11a0766d76fe8d1a57c0dada6f83f
artsbentley <artsbentley@gmail.com>
2025-05-25 12:07:21 +0200

    searxng raycast comment

--- modules/nixos/programs/searxng/default.nix
+++ modules/nixos/programs/searxng/default.nix
@@ -1,4 +1,8 @@
 { vars, config, ... }:
+
+# used for together with raycast quicklink:
+# http://localhost:8888/search?q=!!%20{argument name="term"}
+
 {
   services.searx = {
     enable = true;
@@ -13,3 +17,5 @@
     };
   };
 }
+
+
