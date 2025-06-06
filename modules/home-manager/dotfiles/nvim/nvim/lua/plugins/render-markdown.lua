2b5ceaf23128b4223b95b57042de8bdf85d03444
artsbentley <artsbentley@gmail.com>
2025-06-06 21:36:52 +0200

    wip

--- modules/home-manager/dotfiles/nvim/nvim/lua/plugins/render-markdown.lua
+++ modules/home-manager/dotfiles/nvim/nvim/lua/plugins/render-markdown.lua
@@ -2,7 +2,7 @@ return {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = { "markdown", "Avante" },
     opts = {
-        file_types = { "markdown", "Avante" },
+        file_types = { "markdown", "Avante", "codecompanion" },
         indent = {
             -- Turn on / off org-indent-mode
             enabled = true,
